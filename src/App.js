d4b1ba7679fe8399163c2fab18d78b137ad5d0fd
Danny Chang <dannychachang@gmail.com>
2018-12-05 09:53:16 -0500

    create student component and api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import Student from './components/Students/student';
 import './App.css';
 
 class App extends Component {
+
+  callTheApi = (e) => {
+    fetch('https://www.hatchways.io/api/assessment/students')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      })
+  }
+
   render() {
     return (
       <div className="App">
-
+        <button onClick={this.callTheApi}>Call the Api!</button>
+        <Student />
       </div>
     );
   }
---
+++ src/components/Students/student.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const student = (props) => {
+  return(
+    <div>
+      <h1>hi there</h1>
+    </div>
+  )
+}
+
+export default student;
